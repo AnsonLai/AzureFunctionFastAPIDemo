417da314b9598698f57859ca6d4973a83d2034c7
Anson Lai <ansonhwlai@gmail.com>
2020-08-22 12:34:47 -0700

    tempfile path added

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -3,6 +3,10 @@ from bonnette import Bonnette
 
 from .main import app
 
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+# Option 1: as a StreamingResponse
+from fastapi.responses import StreamingResponse
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     handler = Bonnette(app)
     return handler(req)
--- HttpTrigger1/main.py
+++ HttpTrigger1/main.py
@@ -19,27 +19,31 @@ async def redirect():
     return response
 
 # DOWNLOAD BLOB
-from azure.storage.blob.aio import BlobServiceClient, BlobClient, ContainerClient
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 # Option 1: as a StreamingResponse
 from fastapi.responses import StreamingResponse
+import tempfile
 @app.get("/streamdownload/")
 async def streamdownload():
-    connect_str = "****************************************************************"
+    connect_str = "************"
     blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
-    container_name = "*******************"
+    container_name = "************"
     container_client = blob_service_client.get_container_client(container_name)
 
-    local_file_name = 'test.csv'
+    local_file_name = '************.csv'
 
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
 
-    with open("./temp.csv", "wb") as blob:
-        stream = await blob_client.download_blob()
-        data = await stream.readall()
-        blob.write(data)
+    tempFilePath = tempfile.gettempdir()
+    tempFile = tempfile.NamedTemporaryFile(delete=False)
+    tempFileName = tempFile.name
+    
+    with open(tempFileName, "wb") as blob:
+        data = blob_client.download_blob()
+        data.readinto(blob)
 
-    temp_file = open("./temp.csv", mode="rb")
+    temp_file = open(tempFileName, "rb")
 
     response = StreamingResponse(temp_file, media_type="text/csv")
     response.headers["Content-Disposition"] = "attachment; filename=" + local_file_name
@@ -48,25 +52,60 @@ async def streamdownload():
 
 # Option 2: as a FileResponse
 from fastapi.responses import FileResponse
-@app.get("/filedownload/")
-async def filedownload():
-    connect_str = "****************************************************************"
+@app.get("/filedownload/{filename}")
+async def filedownload(filename: str):
+    connect_str = "************"
     blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
-    container_name = "*******************"
+    container_name = "************"
     container_client = blob_service_client.get_container_client(container_name)
 
-    local_file_name = 'test.csv'
+    local_file_name = filename
 
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
 
-    with open("./temp.csv", "wb") as blob:
-        stream = await blob_client.download_blob()
-        data = await stream.readall()
-        blob.write(data)
+    tempFilePath = tempfile.gettempdir()
+    tempFile = tempfile.NamedTemporaryFile(delete=False)
+    tempFileName = tempFile.name
 
-    response = FileResponse("./temp.csv", media_type="text/csv")
+    with open(tempFileName, "wb") as blob:
+        data = blob_client.download_blob()
+        data.readinto(blob)
+
+    temp_file = open(tempFileName, "rb")
+
+    response = StreamingResponse(temp_file, media_type="text/csv")
     response.headers["Content-Disposition"] = "attachment; filename=" + local_file_name
 
     return response
 
+# Option 3: as a FileResponse (Async) UNCOMMENT IMPORT
+from fastapi.responses import FileResponse
+# from azure.storage.blob.aio import BlobServiceClient, BlobClient, ContainerClient
+@app.get("/filedownload/async/{filename}")
+async def filedownload_aio(filename: str):
+    connect_str = "************"
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
+    container_name = "************"
+    container_client = blob_service_client.get_container_client(container_name)
+
+    local_file_name = filename
+
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
+
+    tempFilePath = tempfile.gettempdir()
+    tempFile = tempfile.NamedTemporaryFile(delete=False)
+    tempFileName = tempFile.name
+
+    with open(tempFileName, "wb") as blob:
+        stream = await blob_client.download_blob()
+        data = await stream.readall()
+        blob.write(data)
+
+    temp_file = open(tempFileName, "rb")
+
+    response = StreamingResponse(temp_file, media_type="text/csv")
+    response.headers["Content-Disposition"] = "attachment; filename=" + local_file_name
+
+    return response
